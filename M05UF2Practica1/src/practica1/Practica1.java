749431b3acc6360381a867ee1b118c0939dd28cb
SamuelReginaldo <usr1@gmail.com>
2023-02-12 21:22:15 +0100

    Afegit la terminació ">" per indicar que es pot escriure

--- M05UF2Practica1/src/practica1/Practica1.java
+++ M05UF2Practica1/src/practica1/Practica1.java
@@ -26,17 +26,19 @@ public class Practica1 {
 
             String numero1;
             do {
-                System.out.println("\n Introdueix el primer numero. ");
+                System.out.println("\nIntrodueix el primer numero. ");
+                System.out.printf(">");
                 numero1 = sc.nextLine();
             } while (!numero1.matches("[+-]?[\\d]*[.]?[\\d]+"));
             double nume1 = Double.parseDouble(numero1);
             double n1 = new Double(numero1);
 
             do {
-                System.out.println("\n Operació? (Indica el signe)");
-                System.out.println("+ = sumar \n - = restar \n"
+                System.out.println("\nOperació? (Indica el signe)");
+                System.out.println(" + = sumar \n - = restar \n"
                         + " x = multiplicar \n / = dividir \n * = elevar primer num al segon num."
                         + "\n % = residu");
+                System.out.printf(">");
                 operacion = sc.nextLine();
                 if (operacion.equals("+") || operacion.equals("-") || operacion.equals("x")
                         || operacion.equals("X") || operacion.equals("/") || operacion.equals("%")
@@ -49,7 +51,8 @@ public class Practica1 {
 
             String numero2;
             do {
-                System.out.println("\n Introdueix el segon numero.");
+                System.out.println("\nIntrodueix el segon numero.");
+                System.out.printf(">");
                 numero2 = sc.nextLine();
             } while (!numero2.matches("[+-]?[\\d]*[.]?[\\d]+"));
             double nume2 = Double.parseDouble(numero2);
@@ -99,8 +102,9 @@ public class Practica1 {
             } while (comprobar != true);
 
             System.out.println("(" + numero1 + ") " + operacion + " (" + numero2 + ")" + " = " + res);
-            System.out.println("\n Vols continuar operant? \n");
-            System.out.println(" [s/n]");
+            System.out.println("\nVols continuar operant?");
+            System.out.println("[s/n]");
+            System.out.printf(">");
             do {
                 comprobar = true;
                 operacion = sc.nextLine();
